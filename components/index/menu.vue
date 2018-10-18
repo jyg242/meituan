22bf843f5f3cfdb9ddb77e887536e7db28e1cfcf
jyg242 <297138663@qq.com>
2018-10-19 01:09:28 +0800

    '1019'

--- components/index/menu.vue
+++ components/index/menu.vue
@@ -1,68 +1,85 @@
 <template>
   <div class="m-menu">
-    <dl class="nav" @mouseleave='mouseleave'>
-      <dt>全部分类</dt>
-      <dd v-for="(item,idx) in menu" :key="idx" @mouseenter='enter'>
+    <dl class="nav" 
+      @mouseleave='mouseleave'>
+      <dt>全部分类</dt> 
+      <dd 
+        v-for="(item,idx) in menu"
+        :key="idx" 
+        @mouseenter='enter'>
         <i :class="item.type"/>{{item.name}}<span class="arrow"></span>
       </dd>
-      <div class="detail" v-if="kind">
-        <template v-for="(item,idx) in curdetail">
-          <h4 :key="idx">{{item.title}}}</h4>
-          <span v-for="v in item.child" :key="v">{{v}}</span>
-        </template> 
-      </div>
-    </dl>
+    </dl>  
+      <div 
+        class="detail" 
+        v-if="kind"
+        @mouseenter='sover'
+        @mouseleave='sout'>
+        <template
+          v-for="(item,idx) in jyg.child">
+          <h4 :key="idx">{{ item.title }}</h4>
+          <span
+          v-for="v in item.child"
+          :key="v">{{ v }}</span>
+      </template>
+      </div> 
   </div>
 </template>
 <script>
 export default {
-  data(){
-    return{  
+    data(){
+    return {
       kind:'',
-      menu:[
-        {
+      jyg:'',
+      menu:[{
         type:'food',
         name:'美食',
         child:[{
           title:'美食',
-          child:['代金券','甜点','饮品','小吃','快餐']
+          child:['代金券','甜点饮品','火锅','自助餐','小吃快餐']
         }]
-        }, {
+      },{
         type:'takeout',
         name:'外卖',
         child:[{
           title:'外卖',
           child:['美团外卖']
         }]
-        }, {
+      },{
         type:'hotel',
         name:'酒店',
         child:[{
           title:'酒店星级',
           child:['经济型','舒适/三星','高档/四星','豪华/五星']
         }]
-        }
-      ]
+      }]
     }
   },
-  computed:{
-    curdetail(){
-      console.log(this.menu.filter(item=>item.type===this.kind)[0])
-      return 
-       this.menu.filter(item=>item.type===this.kind)[0]
-       
-    }
-  },
+  // computed:{
+  //   curdetail(){
+  //     return 
+  //       this.menu.filter(item=>item.type===this.kind)[0]    
+  //   }
+  // },
   methods:{
     mouseleave(){
       let self=this;
-      self._time=setTimeout(function(){
+      self._timer=setTimeout(function(){
         self.kind=''
       },150)
     },
     enter(e){
       this.kind=e.target.querySelector('i').className
       console.log(this.kind)
+      //计算属性用不了只能进行强行赋值的方法
+      this.jyg=this.menu.filter(item=>item.type===this.kind)[0]
+      console.log(this.jyg)
+    },
+    sover(){
+      clearTimeout(this._timer)
+    },
+    sout(){
+      this.kind=''
     }
   }
 }
